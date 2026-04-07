50cbe5e293cbc50336808cc78a2843a7b1577d63
Alhanouf Allazzam <115586263+AlhanoufKMLz@users.noreply.github.com>
2026-04-07 10:56:42 +0300

    exercise 3 done

--- src/Main.java
+++ src/Main.java
@@ -23,7 +23,37 @@ public class Main {
         System.out.print("Longest words are: ");
         printWords(longestWords);//method to print the words
 
-//
+
+
+//        2. Write a program that displays the number of occurrences of an element in the array.
+//        Original Array:
+//        [1,1,1,3,3,5]
+//        Sample Output:
+//        3 occurs 2 times
+//        1 occurs 3 times
+//        9 occurs 0 time
+        System.out.println("-----------------------");
+        System.out.println("EXERCISE #2");
+        System.out.println("-----------------------");
+
+        int[] numbers = {1, 1, 1, 3, 3, 5};
+
+        System.out.print("Please enter number: ");
+        int userNumber = input.nextInt();
+
+        System.out.println("Number " + userNumber + " occurs " + numberOfOccurrences(userNumber,numbers) + " times");
+
+
+        //EXTRA for EX2 - display the number of occurrences for every number in the array
+        ArrayList<Integer>  numbersList = new ArrayList<>();
+        ArrayList<Integer>  occurrences = new ArrayList<>();
+
+        findAllOccurrences(numbers, numbersList, occurrences);
+
+        System.out.println(numbersList);
+        System.out.println(occurrences);
+
+
 
 
 
@@ -57,4 +87,39 @@ public class Main {
                 System.out.print("\"" + words.get(i) + "\", ");
         }
     }
+
+
+    //EXERCISE #2
+    public static int numberOfOccurrences(int number, int[] numbers){
+        int occurrences = 0;
+        for(int n : numbers){
+            if(n == number)
+                occurrences++;
+        }
+        return occurrences;
+    }
+
+    //EXTRA for EX2
+    public static void findAllOccurrences(int[] numbers, ArrayList<Integer> numbersList, ArrayList<Integer>  occurrences){
+        for(int n : numbers){
+            int numberIndex = findInNumbersList(n,numbersList);
+            if( numberIndex == -1){
+                numbersList.add(n);
+                occurrences.add(1);
+            } else {
+                occurrences.set(numberIndex, occurrences.get(numberIndex) + 1);
+            }
+        }
+    }
+
+    public static int findInNumbersList(int number, ArrayList<Integer> numbersList){
+        for(int i = 0; i < numbersList.size(); i++){
+            if(numbersList.get(i) == number){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
 }
